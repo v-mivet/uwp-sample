a5791748f5f883e975c558a3aaacaafe1f7494ef
Mikhail Vetyugov <v-mivet@microsoft.com>
2017-06-01 14:19:21 +0300

    add uwp app

---
+++ UniTests/UnitTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace UniTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var calculator = new Calculator();
+            var result = calculator.Add(1, 2);
+            Assert.AreEqual(result, 3);
+        }
+    }
+}
